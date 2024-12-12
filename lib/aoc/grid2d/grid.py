c922899b4591227598d065e42cbae26592f69160
Jeff Standen <jeff@webgroupmedia.com>
2024-12-11 16:33:19 -0800

    [Library] Added grid2d and vector2d modules

---
+++ lib/aoc/grid2d/grid.py
@@ -0,0 +1,36 @@
+from typing import Callable, List
+from collections import defaultdict
+import lib.aoc.grid2d.vector as vector2d
+
+
+def parse(data: str, as_int: bool = False) -> (defaultdict, dict):
+    return defaultdict(None, {(y, x): int(symbol) if as_int else symbol
+                       for y, row in enumerate(data.splitlines())
+                       for x, symbol in enumerate(list(row))})
+
+
+def render(top_left: tuple, bottom_right: tuple, func: Callable) -> None:
+    for coord in [(y, x) for y in range(*top_left) for x in range(*bottom_right)]:
+        func(coord)
+
+
+def find_coords(grid: defaultdict, value):
+    return set([yx for yx, _ in filter(lambda item: item[1] == value, grid.items())])
+
+
+def neighbors(grid: defaultdict, at: tuple, directions: List[tuple]):
+    results = set()
+    for d in directions:
+        n_at = vector2d.add(at, d)
+        if grid.get(n_at):
+            results.add((n_at, grid[n_at]))
+    return results
+
+
+def fill(top_left: tuple, bot_right: tuple) -> set:
+    x_delta = 1 if bot_right[1] > top_left[1] else -1
+    y_delta = 1 if bot_right[0] > top_left[0] else -1
+    return set([(y, x)
+                for y in range(top_left[0], bot_right[0] + y_delta, y_delta)
+                for x in range(top_left[1], bot_right[1] + x_delta, x_delta)])
+
---
+++ lib/aoc/grid2d/vector.py
@@ -0,0 +1,19 @@
+NESW = ((-1, 0), (0, 1), (1, 0), (0, -1))
+NESW_DIAG = ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))
+
+
+def add(v1, v2):
+    return tuple([v1[n] + v2[n] for n in range(len(v1))])
+
+
+def mul(v1, factor: int):
+    return [v1[i] * factor for i in range(len(v1))]
+
+
+def rotate(from_heading: tuple, side: str) -> tuple:
+    return (-from_heading[1], from_heading[0]) \
+        if 'L' == side else (from_heading[1], -from_heading[0])
+
+
+def slope(v1, v2):
+    return v2[0] - v1[0], v2[1] - v1[1]
