3e67a2c0c91de1bdc4e1a4e0f11cd2bfe4b5cd55
Jeff Standen <jeff@webgroupmedia.com>
2024-12-12 19:57:17 -0800

    [Library] The vector2d library includes an invert() helper and SWNE vector cycles

--- lib/aoc/grid2d/vector.py
+++ lib/aoc/grid2d/vector.py
@@ -1,6 +1,7 @@
 NESW = ((-1, 0), (0, 1), (1, 0), (0, -1))
 NESW_DIAG = ((-1, 0), (0, 1), (1, 0), (0, -1), (-1, 1), (-1, -1), (1, -1), (1, 1))
 
+SWNE = ((1, 0), (0, -1), (-1, 0), (0, 1))
 
 def add(v1, v2):
     return tuple([v1[n] + v2[n] for n in range(len(v1))])
@@ -13,6 +14,10 @@ def mul(v1, factor: int):
 def rotate(from_heading: tuple, side: str) -> tuple:
     return (-from_heading[1], from_heading[0]) \
         if 'L' == side else (from_heading[1], -from_heading[0])
+
+
+def invert(v1: tuple) -> tuple:
+    return -v1[0], -v1[1]
 
 
 def slope(v1, v2):
